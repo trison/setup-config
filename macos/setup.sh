818f2bf58fe11b4eaf671f84d8e3abe8ac7e5a80
Trison Nguyen <trison.ng@gmail.com>
2023-10-12 19:59:41 -0700

    macos: brew pkgs to update script

--- macos/setup.sh
+++ macos/setup.sh
@@ -6,6 +6,9 @@ cp ../.vimrc ~
 
 # Brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew update
+brew instal node
+brew install rar
 
 # XCode command line tools
 xcode-select --install
