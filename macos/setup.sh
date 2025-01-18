d3fcc5c34a57cbc098d53f5d9d7f4b49487fd83d
Trison Nguyen <trison.ng@gmail.com>
2025-01-17 21:16:01 -0800

    linux,macos: setup neovim config

--- macos/setup.sh
+++ macos/setup.sh
@@ -10,11 +10,15 @@ cp ../.vimrc ~
 # Brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew update
-brew instal node
+brew install node
 brew install rar
 brew install blackhole-2ch
 brew install --cask visual-studio-code
 brew install neovim
+brew install ripgrep
+
+# Neovim config from my fork
+git clone https://github.com/trison/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 
 # XCode command line tools
 xcode-select --install
