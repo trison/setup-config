6d1d66f24c76bcfb2adedcebbb5b5afcf8d2b8e2
Trison Nguyen <trison.ng@gmail.com>
2025-01-19 14:35:16 -0800

    linux: alias nvim

--- linux/setup.sh
+++ linux/setup.sh
@@ -14,9 +14,10 @@ apt install gnome-tweaks npm
 # Install Neovim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
-mkdir -p /opt/nvim
-mv nvim.appimage /opt/nvim/nvim
-export PATH="$PATH:/opt/nvim/"
+mkdir -p ~/bin
+mv nvim.appimage ~/bin/nvim
+echo "alias nvim=~/bin/nvim" >> ~/.bashrc
+source ~/.bashrc
 
 # Install Neovim config from my fork
 git clone https://github.com/trison/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
